52e70a41d224090541e30e662f8c763c19f24ace
pradeep8680 <144304161+pradeep8680@users.noreply.github.com>
2023-09-14 09:24:51 +0530

    challenge2.py

--- challenge2.py
+++ challenge2.py
@@ -1,29 +1,29 @@
 class BankAccount:
-def __init__(self, account_number, account_holder_name, initial_balance=0.0):
-  self.__account_number = account_number
-  self.__account_holder_name = account_holder_name
-  self.__account_balance = initial_balance
+  def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+    self.__account_number = account_number
+    self.__account_holder_name = account_holder_name
+    self.__account_balance = initial_balance
 
-def deposit(self, amount):
-  if amount > 0:
-    self._account_balance +=amount
-    print("Deposited ${}. New balance: ${}".format(amount,
+  def deposit(self, amount):
+    if amount > 0:
+      self.__account_balance +=amount
+      print("Deposited ${}. New balance: ${}".format(amount,
                                                    self.__account_balance))
-  else:
-    print("Invalid deposit amount. please deposit a positive amount.")
+    else:
+      print("Invalid deposit amount. please deposit a positive amount.")
 
-def withdraw(self, amount):
-  if amount > 0 and amount <= self.__account_balance:
-    self.__account_balance -= amount
-    print("Withdrew ${}. New balance: ${}".format(amount,
-                                                  self.__account_balance.))
-  else:
-    print("Invalid Withdrawal amount or insufficient balance.")
+  def withdraw(self, amount):
+    if amount > 0 and amount <= self.__account_balance:
+      self.__account_balance -= amount
+      print("Withdrew ${}. New balance: ${}".format(amount,
+                                                  self.__account_balance))
+    else:
+      print("Invalid Withdrawal amount or insufficient balance.")
 
-def display_balance(self):
-  print("Account balance for {} (Account #{}): ${}".format(
-    self.__account_holder_name,self.__account_number,
-    self.__account_balance))
+  def display_balance(self):
+     print("Account balance for {} (Account #{}): ${}".format(
+     self.__account_holder_name,self.__account_number,
+     self.__account_balance))
 
 
 
